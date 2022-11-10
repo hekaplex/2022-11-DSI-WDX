06c57ea5f0650ded2bd2d1854b24f373ea8e19f3
drew.minkin <drew.minkin@divergence.one>
2022-11-10 14:57:32 -0600

    windowing function

---
+++ instructor/sql/2022-11-DSI-WDX In Class Code-Along/Best FG Percentage Nov 10.sql
@@ -0,0 +1,23 @@
+--Best pctg FG player by Team
+with base as
+(
+select
+[Teams], [Players], [FG]
+from
+[dbo].[BSN_playersStats]
+where [Players] is not null
+)
+,
+window
+as
+(select 
+	*
+	,row_number() 
+	--windowing function
+	over (---group by for the query
+			partition by teams 
+			---aggregate criteria per tuple
+			order by FG desc) as fg_rank
+from base)
+select [Teams], [Players], [FG]
+from window where fg_rank = 1
