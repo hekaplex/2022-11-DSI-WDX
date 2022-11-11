ee9d6bee82dfc7ab66acfc4a4961b5aea43a7e83
drew.minkin <drew.minkin@divergence.one>
2022-11-11 12:36:20 -0600

    Nov 11 in classs SQL

--- instructor/sql/2022-11-DSI-WDX In Class Code-Along/Best FG Percentage Nov 10.sql
+++ instructor/sql/2022-11-DSI-WDX In Class Code-Along/Best FG Percentage Nov 10.sql
@@ -1,4 +1,12 @@
 --Best pctg FG player by Team
+--CTE expression
+--who is the best FG% in a team?
+
+/*
+0. Team - Player - FG% - output column
+1. build a rank by team and know where every player stands
+2. best is where rank = 1
+*/
 with base as
 (
 select
@@ -7,6 +15,7 @@ from
 [dbo].[BSN_playersStats]
 where [Players] is not null
 )
+
 ,
 window
 as
@@ -21,3 +30,36 @@ as
 from base)
 select [Teams], [Players], [FG]
 from window where fg_rank = 1
+
+
+
+--Best pctg FG player by Team
+--old school - correlated subqueries
+select 
+	[Teams]
+	, [Players]
+	, [FG]
+from 
+	(
+	--ranking per team for each player
+		select 
+			base.*
+			,row_number() 
+			--windowing function
+				over (---group by for the query
+					partition by base.teams 
+			---aggregate , in our case rank, criteria per tuple
+					order by base.FG desc) 
+				as fg_rank
+		from 
+			(
+				select
+				[Teams], [Players], [FG]
+				from
+				[dbo].[BSN_playersStats]
+				where [Players] is not null
+			) as base
+) window
+where fg_rank = 1
+
+
--- instructor/sql/2022-11-DSI-WDX In Class Code-Along/Shaping with Simple Tables Nov 10.sql
+++ instructor/sql/2022-11-DSI-WDX In Class Code-Along/Shaping with Simple Tables Nov 10.sql
@@ -5,6 +5,8 @@ KeyCol int,
 TblName char(20),
 ColValue int
 )
+truncate table Table1
+truncate table Table2
 GO
 CREATE TABLE [Table2]
 (
@@ -25,8 +27,8 @@ INSERT INTO
 	[Table1] 
 	(KeyCol,TblName,ColValue) 
 	VALUES
-		 (1,'Table2',1) 
-		,(2,'Table2',1) 
+		 (1,'Table1',1) 
+		,(2,'Table1',1) 
 GO
 */
 SELECT 
@@ -77,6 +79,7 @@ JOIN
 	Table2 b
 ON
 	a.KeyCol = b.KeyCol
+GO
 SELECT 
 	 a.*
 	,b.*,'Cross Join' as ShapeType
@@ -87,10 +90,60 @@ GO
 SELECT 
 	 a.*,'Union' as ShapeType
 FROM Table1 a
-UNION
+UNION --remove duplicates
+--UNION ALL will not remove duplicates
 SELECT 
 	b.*,'Union' as ShapeType
 FROM
 	Table2 b
 GO
 
+ALTER FUNCTION apply_tbl(@Keycol int)
+RETURNS TABLE
+RETURN
+	SELECT * from Table1 where ColValue = @Keycol 
+GO
+select 'sample use of TVF' as Example,* from apply_tbl(1)
+GO
+--CROSS APPLY
+SELECT 
+	* 
+, 'Cross Apply' as Shape
+from Table2 
+CROSS APPLY  apply_tbl(KeyCol)
+GO
+--CROSS APPLY
+SELECT 
+	* 
+, 'Outer Apply' as Shape
+from Table2 
+Outer APPLY  apply_tbl(KeyCol)
+
+
+SELECT 'Sum_ColVal' AS Pivot_Example,   
+  [1], [3], [4]  
+FROM  
+(
+  SELECT KeyCol,ColValue
+  FROM Table2
+) AS SourceTable  
+PIVOT  
+(  
+  SUM(ColValue)
+  FOR KeyCol IN ( [1], [3], [4])  
+) AS PivotTable;
+GO
+
+
+SELECT TblName, UnpivotColName, UnpivotColVal
+FROM   
+   (SELECT KeyCol, TblName, ColValue
+   FROM Table2
+   UNION
+   SELECT KeyCol, TblName, ColValue
+   FROM Table1
+   ) p  
+UNPIVOT  
+   (UnpivotColVal FOR UnpivotColName IN   
+      (KeyCol)  
+)AS unpvt; 
