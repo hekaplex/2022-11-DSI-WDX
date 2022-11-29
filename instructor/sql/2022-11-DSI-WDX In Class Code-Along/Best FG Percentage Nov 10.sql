1a6972206509eb74a95e09c48aba7770d89bf609
drew.minkin <drew.minkin@divergence.one>
2022-11-29 09:09:38 -0600

    PL-300 instructions

--- instructor/sql/2022-11-DSI-WDX In Class Code-Along/Best FG Percentage Nov 10.sql
+++ instructor/sql/2022-11-DSI-WDX In Class Code-Along/Best FG Percentage Nov 10.sql
@@ -1,12 +1,4 @@
 --Best pctg FG player by Team
---CTE expression
---who is the best FG% in a team?
-
-/*
-0. Team - Player - FG% - output column
-1. build a rank by team and know where every player stands
-2. best is where rank = 1
-*/
 with base as
 (
 select
@@ -15,7 +7,6 @@ from
 [dbo].[BSN_playersStats]
 where [Players] is not null
 )
-
 ,
 window
 as
@@ -34,32 +25,25 @@ from window where fg_rank = 1
 
 
 --Best pctg FG player by Team
---old school - correlated subqueries
-select 
-	[Teams]
-	, [Players]
-	, [FG]
-from 
-	(
-	--ranking per team for each player
-		select 
-			base.*
-			,row_number() 
-			--windowing function
-				over (---group by for the query
-					partition by base.teams 
-			---aggregate , in our case rank, criteria per tuple
-					order by base.FG desc) 
-				as fg_rank
-		from 
-			(
-				select
-				[Teams], [Players], [FG]
-				from
-				[dbo].[BSN_playersStats]
-				where [Players] is not null
-			) as base
-) window
-where fg_rank = 1
-
-
+with base as
+,
+window
+as
+(select 
+	*
+	,row_number() 
+	--windowing function
+	over (---group by for the query
+			partition by teams 
+			---aggregate criteria per tuple
+			order by FG desc) as fg_rank
+from (
+select
+[Teams], [Players], [FG]
+from
+[dbo].[BSN_playersStats]
+where [Players] is not null
+)
+ base)
+select [Teams], [Players], [FG]
+from window where fg_rank = 1
