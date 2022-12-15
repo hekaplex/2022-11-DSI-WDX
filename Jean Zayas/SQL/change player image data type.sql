123c0c137d83a117c189444f7b6fdbf1bafe745d
theCurador <jean.zayas14@gmail.com>
2022-12-15 13:23:42 -0400

    match player image with names and jerseys

--- Jean Zayas/SQL/change player image data type.sql
+++ Jean Zayas/SQL/change player image data type.sql
@@ -4,56 +4,55 @@ ALTER TABLE [Atleticos Roster] ALTER COLUMN Player_Image varchar(255)
 
 
 	   update [Atleticos Roster] set Player_Image = 'https://www.bsnpr.com/uploads/equipos/E7.png'
-	 where Player_Numbers = 1 --  nate mason jr
+	 where Player_Numbers = 1 AND Last_Name LIKE '%ason jr'  
 
 	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13079.jpg'
-	 where Player_Numbers = 2 --  tjader fernandez
+	 where Player_Numbers = 2 AND Last_Name LIKE '%ernandez'
 
 	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J12964.png'
-	 where Player_Numbers = 6 --  will daniels
+	 where Player_Numbers = 6 AND Last_Name LIKE '%aniels'
 
 	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13233.jpg'
-	 where Player_Numbers = 7 --  josue erazo
+	 where Player_Numbers = 7 AND Last_Name LIKE '%razo'
 
 	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13398.jpg'
-	 where Player_Numbers = 10 --  glenn sanabria
+	 where Player_Numbers = 10 AND Last_Name LIKE '%anabria'
 
 	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13320.jpg'
-	 where Player_Numbers = 11 --  kenneth santos
+	 where Player_Numbers = 11 AND Last_Name LIKE '%antos'
 
 	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J2780.jpg'
-	 where Player_Numbers = 12 --  jorge brian diaz
+	 where Player_Numbers = 12 AND Last_Name LIKE '%iaz'
 
 	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/equipos/E7.png'
-	 where Player_Numbers = 13 --  sheldon mac
+	 where Player_Numbers = 13 AND Last_Name LIKE '%ac'
 
 	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/equipos/E7.png'
-	 where Player_Numbers = 14 --  rondae hollis jefferson
+	 where Player_Numbers = 14 AND Last_Name LIKE '%ollis jefferson'
 
 	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J12969.jpg'
-	 where Player_Numbers = 15 --  onzie branch
+	 where Player_Numbers = 15 AND Last_Name LIKE '%ranch'
 
 	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13340.jpg'
-	 where Player_Numbers = 16 --  pedro villaman
+	 where Player_Numbers = 16 AND Last_Name LIKE '%illaman'
 
 	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13393.jpg'
-	 where Player_Numbers = 23 --  goree venable gonzalez
+	 where Player_Numbers = 23 AND Last_Name LIKE '%onzalez'--  goree venable gonzalez
 
 	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/equipos/E7.png'
-	 where Player_Numbers = 23 --  jorge sanabria
+	 where Player_Numbers = 23 AND Last_Name LIKE '%anabria' --  jorge sanabria
 
 	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13323.jpg'
-	 where Player_Numbers = 24 --  javier quinones
+	 where Player_Numbers = 24 AND Last_Name LIKE '%uinones'
 
 	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J2637.jpg'
-	 where Player_Numbers = 35 --  luis hernandez
+	 where Player_Numbers = 35 AND Last_Name LIKE '%ernandez'
 
 	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13055.jpg'
-	 where Player_Numbers = 45 --  jose g rodriguez
+	 where Player_Numbers = 45 AND Last_Name LIKE '%odriguez'
 
 	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/equipos/E7.png'
-	 where Player_Numbers = 55 --  eric ayala
+	 where Player_Numbers = 55 AND Last_Name LIKE '%yala'
 
 	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J2476.png'
-	 where Player_Numbers = 77 --  angel junito alamo
-
+	 where Player_Numbers = 77 AND Last_Name LIKE '%lamo'
