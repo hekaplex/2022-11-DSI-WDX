7d452abdd2f19d6de7e726c24e4cb8818d009186
theCurador <jean.zayas14@gmail.com>
2022-12-15 01:18:52 -0400

    cleaning urls

--- Jean Zayas/SQL/change player image data type.sql
+++ Jean Zayas/SQL/change player image data type.sql
@@ -3,57 +3,57 @@ ALTER TABLE [Atleticos Roster] ALTER COLUMN Player_Image varchar(255)
 
 
 
-	   update [Atleticos Roster] set Player_Image = 'https://www.bsnpr.com/uploads/equipos/E7.png?n=0.4209406'
+	   update [Atleticos Roster] set Player_Image = 'https://www.bsnpr.com/uploads/equipos/E7.png'
 	 where Player_Numbers = 1 --  nate mason jr
 
-	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13079.jpg?n=0.4209406'
+	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13079.jpg'
 	 where Player_Numbers = 2 --  tjader fernandez
 
-	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J12964.png?n=0.4209406'
+	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J12964.png'
 	 where Player_Numbers = 6 --  will daniels
 
-	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13233.jpg?n=0.4209406'
+	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13233.jpg'
 	 where Player_Numbers = 7 --  josue erazo
 
-	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13398.jpg?n=0.4209406'
+	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13398.jpg'
 	 where Player_Numbers = 10 --  glenn sanabria
 
-	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13320.jpg?n=0.4209406'
+	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13320.jpg'
 	 where Player_Numbers = 11 --  kenneth santos
 
-	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J2780.jpg?n=0.4209406'
+	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J2780.jpg'
 	 where Player_Numbers = 12 --  jorge brian diaz
 
-	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/equipos/E7.png?n=0.4209406'
+	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/equipos/E7.png'
 	 where Player_Numbers = 13 --  sheldon mac
 
-	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/equipos/E7.png?n=0.4209406'
+	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/equipos/E7.png'
 	 where Player_Numbers = 14 --  rondae hollis jefferson
 
-	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J12969.jpg?n=0.4209406'
+	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J12969.jpg'
 	 where Player_Numbers = 15 --  onzie branch
 
-	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13340.jpg?n=0.4209406'
+	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13340.jpg'
 	 where Player_Numbers = 16 --  pedro villaman
 
-	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13393.jpg?n=0.4209406'
+	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13393.jpg'
 	 where Player_Numbers = 23 --  goree venable gonzalez
 
-	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/equipos/E7.png?n=0.4209406'
+	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/equipos/E7.png'
 	 where Player_Numbers = 23 --  jorge sanabria
 
-	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13323.jpg?n=0.4209406'
+	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13323.jpg'
 	 where Player_Numbers = 24 --  javier quinones
 
-	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J2637.jpg?n=0.4209406'
+	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J2637.jpg'
 	 where Player_Numbers = 35 --  luis hernandez
 
-	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13055.jpg?n=0.4209406'
+	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J13055.jpg'
 	 where Player_Numbers = 45 --  jose g rodriguez
 
-	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/equipos/E7.png?n=0.4209406'
+	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/equipos/E7.png'
 	 where Player_Numbers = 55 --  eric ayala
 
-	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J2476.png?n=0.4209406'
+	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J2476.png'
 	 where Player_Numbers = 77 --  angel junito alamo
 
