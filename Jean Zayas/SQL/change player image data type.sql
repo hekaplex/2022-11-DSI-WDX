5557f858c6dd71918eef544361bc77850f9d8722
theCurador <jean.zayas14@gmail.com>
2022-12-16 13:42:13 -0400

    Added team logos and also deleted empty rows

--- Jean Zayas/SQL/change player image data type.sql
+++ Jean Zayas/SQL/change player image data type.sql
@@ -56,3 +56,47 @@ ALTER TABLE [Atleticos Roster] ALTER COLUMN Player_Image varchar(255)
 
 	update [Atleticos Roster] set Player_Image =  'https://www.bsnpr.com/uploads/jugadores_small/J2476.png'
 	 where Player_Numbers = 77 AND Last_Name LIKE '%lamo'
+
+
+ALTER TABLE [BSN_TEAMS] ALTER COLUMN Logo varchar(255)
+ 
+DELETE FROM [BSN_TEAMS] WHERE Teams IS NULL
+
+ update [BSN_TEAMS] set Logo = 'https://www.bsnpr.com/uploads/equipos/E7.png'
+	 where Teams LIKE '%tleticos'  --Atleticos
+
+ update [BSN_TEAMS] set Logo = 'https://www.bsnpr.com/uploads/equipos/E13.png'
+	where Teams LIKE '%rujos' --Brujos
+	
+ update [BSN_TEAMS] set Logo = 'https://www.bsnpr.com/uploads/equipos/E1.png'
+	where Teams LIKE '%angrejeros' --Cangrejeros
+
+ update [BSN_TEAMS] set Logo = 'https://www.bsnpr.com/uploads/equipos/E2.png'
+	where Teams LIKE '%apitanes' --Capitanes
+
+ update [BSN_TEAMS] set Logo = 'https://www.bsnpr.com/uploads/equipos/E15.png'
+	where Teams LIKE '%ariduros' --Cariduros
+
+ update [BSN_TEAMS] set Logo = 'https://www.bsnpr.com/uploads/equipos/E10.png'
+	where Teams LIKE '%igantes' --Gigantes
+
+ update [BSN_TEAMS] set Logo = 'https://www.bsnpr.com/uploads/equipos/E73.png'
+	where Teams LIKE '%rises' --Grises
+
+ update [BSN_TEAMS] set Logo = 'https://www.bsnpr.com/uploads/equipos/E12.png'
+	where Teams LIKE '%ndios' --Indios
+
+ update [BSN_TEAMS] set Logo = 'https://www.bsnpr.com/uploads/equipos/E5.jpg'
+	where Teams LIKE '%eones' --Leones
+
+ update [BSN_TEAMS] set Logo = 'https://www.bsnpr.com/uploads/equipos/E14.png'
+	where Teams LIKE '%ets' --Mets
+
+ update [BSN_TEAMS] set Logo = 'https://www.bsnpr.com/uploads/equipos/E6.png'
+	where Teams LIKE '%iratas' --Piratas
+
+ update [BSN_TEAMS] set Logo = 'https://www.bsnpr.com/uploads/equipos/E9.png'
+ 	where Teams LIKE '%aqueros' --Vaqueros
+
+
+SELECT * FROM BSN_TEAMS
