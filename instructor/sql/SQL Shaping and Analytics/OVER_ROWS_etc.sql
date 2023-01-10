681e3b6748e0ff1df62deed03bae0462c135a59e
drew.minkin <drew.minkin@divergence.one>
2023-01-10 12:10:13 -0600

    Additional Shaping Samples

---
+++ instructor/sql/SQL Shaping and Analytics/OVER_ROWS_etc.sql
@@ -0,0 +1,26 @@
+SELECT BusinessEntityID, TerritoryID   
+    ,CONVERT(VARCHAR(20),SalesYTD,1) AS SalesYTD  
+    ,DATEPART(yy,ModifiedDate) AS SalesYear  
+    ,CONVERT(VARCHAR(20),SUM(SalesYTD) OVER (PARTITION BY TerritoryID   
+                                             ORDER BY DATEPART(yy,ModifiedDate)   
+                                             ROWS BETWEEN CURRENT ROW AND 1 FOLLOWING ),1) AS CumulativeTotal  
+FROM Sales.SalesPerson  
+WHERE TerritoryID IS NULL OR TerritoryID < 5;
+
+SELECT BusinessEntityID, TerritoryID   
+    ,CONVERT(VARCHAR(20),SalesYTD,1) AS SalesYTD  
+    ,DATEPART(yy,ModifiedDate) AS SalesYear  
+    ,CONVERT(VARCHAR(20),SUM(SalesYTD) OVER (PARTITION BY TerritoryID   
+                                             ORDER BY DATEPART(yy,ModifiedDate)   
+                                             ROWS BETWEEN 1 PRECEDING AND CURRENT ROW ),1) AS CumulativeTotal  
+FROM Sales.SalesPerson  
+WHERE TerritoryID IS NULL OR TerritoryID < 5;
+
+SELECT BusinessEntityID, TerritoryID   
+    ,CONVERT(VARCHAR(20),SalesYTD,1) AS SalesYTD  
+    ,DATEPART(yy,ModifiedDate) AS SalesYear  
+    ,CONVERT(VARCHAR(20),SUM(SalesYTD) OVER (PARTITION BY TerritoryID   
+                                             ORDER BY DATEPART(yy,ModifiedDate)   
+                                             ROWS UNBOUNDED PRECEDING),1) AS CumulativeTotal  
+FROM Sales.SalesPerson  
+WHERE TerritoryID IS NULL OR TerritoryID < 5;
